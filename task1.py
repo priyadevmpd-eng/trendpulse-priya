ec011a3327a050c41ccaff404fec17224dcac754
priyadevmpd-eng <priyadevmpd@gmail.com>
2026-04-02 16:38:28 +0530

    Delete task1.py

--- task1.py
+++
@@ -1,7 +0,0 @@
-import requests
-url = "https://www.reddit.com/r/news/hot.json"
-headers = {
-    "User-Agent": "python:trendpulse:v1.0 (by /u/testuser)"
-}
-response = requests.get(url,headers=headers)
-print(response.status_code)
